66ee2036fcc92a03d2490230cf6ae964af6e0862
Ezequiel <zeki-san@hotmail.com>
2017-01-27 13:43:42 -0200

    oi

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 
 gem 'figaro'
+gem 'net-ssh'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,8 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
 
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/blog2.pem) }
+
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
